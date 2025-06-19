5615f9c462d4205eb260fa096cee689fba3c78d7
SUITE <DW1@mail.com>
2025-06-19 15:22:22 -0500

    Libraries and permission has been setup.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,28 @@ android {
 
 dependencies {
 
+    //Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    //OKHTTP
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    //Courtines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    //ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+
+    //Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+
+    //Coil
+    implementation("io.coil-kt:coil-compose:2.5.0")
+
+    // Material components
+    implementation("androidx.compose.material:material:1.5.4")
+
+    //Gson
+    implementation("com.google.code.gson:gson:2.10.1")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
